be549b8c5d1a470dde0a6d288fafad365e9cbdb1
WhiredPlanck <whiredplanck@outlook.com>
2024-01-19 23:07:13 +0800

    Move the modified AndroLua source code
    Various errors were initially fixed.

---
+++ androlua/src/main/java/nirenr/androlua/LuaContentObserver.java
@@ -0,0 +1,78 @@
+package nirenr.androlua;
+
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Created by nirenr on 2017/11/09 0009.
+ */
+
+public class LuaContentObserver extends ContentObserver implements LuaGcable{
+
+    private OnChangeListener mOnChangeListener;
+    private boolean mGc;
+
+    private LuaContentObserver(Handler handler) {
+        super(handler);
+    }
+
+    public LuaContentObserver(@NonNull LuaContext context, String uri) {
+        this(new Handler(LuaApplication.getInstance().getMainLooper()));
+        Uri mUri = Uri.parse(uri);
+        context.regGc(this);
+        LuaApplication.getInstance().getContentResolver().registerContentObserver(
+                mUri,
+                true,
+                this
+        );
+    }
+
+
+    public LuaContentObserver(@NonNull LuaContext context, Uri mUri) {
+        this(new Handler(LuaApplication.getInstance().getMainLooper()));
+
+        context.regGc(this);
+        LuaApplication.getInstance().getContentResolver().registerContentObserver(
+                mUri,
+                true,
+                this
+        );
+    }
+
+    @Override
+    public void onChange(boolean selfChange, Uri uri) {
+        super.onChange(selfChange, uri);
+        if (mOnChangeListener != null){
+            Cursor cursor = LuaApplication.getInstance().getContentResolver().query(uri, null, null, null, null);
+            if(cursor != null)
+                cursor.moveToFirst();
+            mOnChangeListener.onChange(selfChange, uri,cursor);
+            if(cursor != null)
+               cursor.close();
+        }
+    }
+
+    public void setOnChangeListener(OnChangeListener listener) {
+        mOnChangeListener = listener;
+    }
+
+    @Override
+    public void gc() {
+        LuaApplication.getInstance().getContentResolver().unregisterContentObserver(this);
+        mGc=true;
+    }
+
+    @Override
+    public boolean isGc() {
+        return mGc;
+    }
+
+    public interface OnChangeListener {
+        public void onChange(boolean selfChange, Uri uri,Cursor cursor);
+    }
+
+}
---
+++ androlua/src/main/java/nirenr/androlua/LuaGcable.java
@@ -0,0 +1,7 @@
+package nirenr.androlua;
+
+public interface LuaGcable
+{
+	public void gc();
+	public boolean isGc();
+}
